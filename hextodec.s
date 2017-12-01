708d279878cbf35b04e27afc264abacf0b80acff
Temi <temilola.oloyede@bison.howard.edu>
2017-12-01 01:56:33 -0500

    Read and process input

--- hextodec.s
+++ hextodec.s
@@ -2,26 +2,61 @@
 # of an hexadecimal number specified by user input
 .data
 str: .space 9
+newstr: .space 1000
 error: .asciiz "Invalid hexadecimal number."
 buffer: .space 11	# buffer string to store decimal string
 
 .text
 main:
-addi $s0, $zero, 0    	# Initialize $s0
+addi $s0, $zero, 0    			# Initialize $s0
 
-li $v0, 8 							# Read in hexadecimal number
+li $v0, 8 						# Read in hexadecimal number
 la $a0, str
-li $a1, 9							# of size 9 
+li $a1, 9						# of size 9 
 syscall
+
+li $v0, 8 						# Read in hexadecimal numbers
+la $a0, newstr
+li $a1, 1000					# of size 1000 
+syscall
+
+la $s4, newstr					# Load address of newstr
+anotherloop:
+add $t4, $s4, $zero				# Copy the address of newstr to $t4
+lbu $t3, 0($t4)					# Load character at $t4
+bne $t3, $zero, checkenter		# Exit loop if the null character is encountered
+j continue
+checkenter:
+addi $t7, $zero, 10 
+bne $t3, $t7, innerloop			# Exit loop if enter character is encountered
+j continue
+
+innerloop:
+lbu $t3, 0($t4)					# Load character at $t4
+addi $t7, $zero, 44				# Load the ascii value of comma for comparison
+bne $t3, $t7, notcomma			# Check if the ascii value is comma
+add $a0, $s4, $zero				# Add address of string to argument for hex_string function
+jal hex_string					# Call function to convert hexadecimal string to integer
+lw $s0, 0($sp)					# Copy return value from stack to $s0
+addi $sp, $sp, 4				# Increment stack pointer by 4
+# Print string
+# Print comma
+j anotherloop
+notcomma:
+addi $t3, $t3, 1				# Check the next value
+j innerloop
+
+continue:						# Exit anotherloop
+#Exit program ideally
 
 la $s1, str
 
-add $a0, $s1, $zero					# Add address of string to argument
+add $a0, $s1, $zero				# Add address of string to argument
 jal hex_string
-lw $s0, 0($sp)						# Copy return value from stack to $s0
-addi $sp, $sp, 4					# Increment stack pointer by 4
+lw $s0, 0($sp)					# Copy return value from stack to $s0
+addi $sp, $sp, 4				# Increment stack pointer by 4
 
-#li $v0, 1								# Print integer value
+#li $v0, 1						# Print integer value
 #add $a0, $s0, $zero			# in $s0
 #syscall
 
@@ -35,10 +70,10 @@ stringloop: addi $t6, $zero, 1
 sub $s3, $s3, $t6				# Subtract 1 from s3
 addi $t6, $zero, 10
 divu $s0, $t6					# Divide the decimal in $s0 by 10
-mfhi $t7							# Put the remainder in $t7
-mflo $s0							# Put the quotient in $s0
-addi $t7, $t7, 48 		# Convert decimal in $t7 to ascii
-sb $t7, 0($s3)   			# store remainder in $s3
+mfhi $t7						# Put the remainder in $t7
+mflo $s0						# Put the quotient in $s0
+addi $t7, $t7, 48 				# Convert decimal in $t7 to ascii
+sb $t7, 0($s3)   				# store remainder in $s3
 
 bne $s0, $zero, stringloop		# Continue looping until quotient is 0
 
@@ -48,8 +83,51 @@ li $v0, 4  						# Print string
 la $a0, 0($s3)
 syscall
 
-li $v0, 10 							# Exit Program
+li $v0, 10 						# Exit Program
 syscall
+
+########################################print_decimal
+# Function prints unsigned decimal value of argument
+#
+# Arg registers used: none
+# Tmp registers used: $t0, $t6, $t7
+#
+# Pre: 0($sp) contains the argument value
+# Post: none
+# Returns: none
+#
+# Called by: main
+# Calls: none
+la $t0, buffer
+
+lw $t2, 0($sp)					# Copy argument from stack
+addi $sp, $sp, 4				# Increment the stack
+
+# write buffer spaces to all space
+
+addi $t7, $zero, 0
+sb $t7, 8($t0)					# Null terminate the buffer string
+
+addi $t0, $t0, 8				# Start from the end of the buffer string
+
+stringloop: addi $t6, $zero, 1
+sub $t0, $t0, $t6				# Subtract 1 from s3
+addi $t6, $zero, 10
+divu $s0, $t6					# Divide the decimal in $s0 by 10
+mfhi $t7						# Put the remainder in $t7
+mflo $s0						# Put the quotient in $s0
+addi $t7, $t7, 48 				# Convert decimal in $t7 to ascii
+sb $t7, 0($t0)   				# store remainder in $s3
+
+bne $s0, $zero, stringloop		# Continue looping until quotient is 0
+
+finalexit: 
+
+li $v0, 4  						# Print string
+la $a0, 0($t0)
+syscall
+
+jr $ra							# Exit function
 
 ########################################hex_string
 # Function returns decimal value of an hexadecimal string
@@ -64,50 +142,50 @@ syscall
 # Called by: main
 # Calls: hex_funct
 hex_string:
-add $t9, $a0, $zero			# Load address of string from argument
+add $t9, $a0, $zero				# Load address of string from argument
 lbu $t2, 0($t9)					# Load character at $t9
-addi $t1, $zero, 0			# initialize counter $t1 to zero
-add $t5, $ra, $zero 		# Store the value in $ra into $t5
+addi $t1, $zero, 0				# initialize counter $t1 to zero
+add $t5, $ra, $zero 			# Store the value in $ra into $t5
 
 sp: addi $t7, $zero, 32			# Store 32 - ascii space in $t7			
-bne $t2, $t7, loop					# If not space branch to loop
+bne $t2, $t7, loop				# If not space branch to loop
 addi $t9, $t9, 1				
-lbu $t2, 0($t9)				# Load the next charcter
-j sp						# Repeat until character is not space
+lbu $t2, 0($t9)					# Load the next charcter
+j sp							# Repeat until character is not space
 
 loop: 
-add $a0, $zero,  $t2		# Initialize values for function: hex_funct
+add $a0, $zero,  $t2			# Initialize values for function: hex_funct
 jal hex_funct
-addi $t7, $zero, 1			# Set temporary variable to 1
-sub $t3, $zero, $t7			# Set temporary variable to -1
-bne $v0, $t3, valid			# Check whether $v0 is not equal to -1
+addi $t7, $zero, 1				# Set temporary variable to 1
+sub $t3, $zero, $t7				# Set temporary variable to -1
+bne $v0, $t3, valid				# Check whether $v0 is not equal to -1
 
-li $v0, 4					# Print error string, the input character is not a hex value
+li $v0, 4						# Print error string, the input character is not a hex value
 la $a0, error
 syscall
-li $v0, 10 					# Exit Program
+li $v0, 10 						# Exit Program
 syscall
 
 valid: add $t0, $v0, $zero
-bne $t1, $zero, else		# If it's the first number, don't shift the register
+bne $t1, $zero, else			# If it's the first number, don't shift the register
 j both
-else: sll $t6, $t6, 4   	# Shift register that stores the decimal number by 4
-both: add $t6, $t6, $t0		# Add the new decimal returned by hex_funct to the number
+else: sll $t6, $t6, 4   		# Shift register that stores the decimal number by 4
+both: add $t6, $t6, $t0			# Add the new decimal returned by hex_funct to the number
 
-addi $t1, $t1, 1			# Update the counter
+addi $t1, $t1, 1				# Update the counter
 
 addi $t9, $t9, 1
-lbu $t2, 0($t9)				# Load the next  character
-beq $t2, $zero, finish 		# Exit loop when next character in string is null
+lbu $t2, 0($t9)					# Load the next  character
+beq $t2, $zero, finish 			# Exit loop when next character in string is null
 addi $t8, $zero, 10
-beq $t2, $t8, finish		# Exit loop when next character in string is enter
+beq $t2, $t8, finish			# Exit loop when next character in string is enter
 j loop
 
 finish:
-addi $sp, $sp, -4			# Decrement stack pointer by 4
-sw $t6, 0($sp)				# Copy $t6 to stack
+addi $sp, $sp, -4				# Decrement stack pointer by 4
+sw $t6, 0($sp)					# Copy $t6 to stack
 
-add $ra, $t5, $zero		# Reload $ra from t5
+add $ra, $t5, $zero				# Reload $ra from t5
 jr $ra
 
 
@@ -125,42 +203,42 @@ jr $ra
 # Calls: none
 hex_funct:	
 addi $t7, $zero, 1								
-sub $v0, $zero, $t7			# Initialize $v0 to -1
+sub $v0, $zero, $t7				# Initialize $v0 to -1
 
-li $t0, 48							# Initialize lower bound for the ascii range 0 - 9
-li $t2, 58							# Initialize upper bound the ascii range 0 - 9
-add $t3, $t0, $zero			# t3 is the value to be subtracted from the ascii value to get its value in hex
+li $t0, 48						# Initialize lower bound for the ascii range 0 - 9
+li $t2, 58						# Initialize upper bound the ascii range 0 - 9
+add $t3, $t0, $zero				# t3 is the value to be subtracted from the ascii value to get its value in hex
 
-l1: bne $a0, $t0, ne1 	# a0 is the ascii value, t0 is the lower bound of the ascii range to be checked
+l1: bne $a0, $t0, ne1 			# a0 is the ascii value, t0 is the lower bound of the ascii range to be checked
 sub $v0, $t0, $t3				# t3 is the value to be subtracted from the ascii value to get its value in hex
 jr $ra
 ne1: addi $t0, $t0, 1
-bne $t0, $t2, nne1			# t2 is the upper bound of the ascii range plus 1
-j label1								# Hex value not in range, try next range
+bne $t0, $t2, nne1				# t2 is the upper bound of the ascii range plus 1
+j label1						# Hex value not in range, try next range
 nne1: j l1
 
-label1: li $t0, 65			# Initialize lower bound for the ascii range A - F
-li $t2, 71							# Initialize upper bound for the ascii range A - F
-addi $t4, $zero, 10			# Set temporary variable to 10
+label1: li $t0, 65				# Initialize lower bound for the ascii range A - F
+li $t2, 71						# Initialize upper bound for the ascii range A - F
+addi $t4, $zero, 10				# Set temporary variable to 10
 sub $t3, $t0, $t4				# t3 is the value to be subtracted from the ascii value to get its value in hex
 
-l2: bne $a0, $t0, ne2 	# a0 is the ascii value, t0 is the lower bound of the ascii range to be checked
+l2: bne $a0, $t0, ne2 			# a0 is the ascii value, t0 is the lower bound of the ascii range to be checked
 sub $v0, $t0, $t3				# t3 is the value to be subtracted from the ascii value to get its value in hex
 jr $ra
 ne2: addi $t0, $t0, 1
-bne $t0, $t2, nne2			# t2 is the upper bound of the ascii range plus 1
-j label2								# Hex value not in range, try next range
+bne $t0, $t2, nne2				# t2 is the upper bound of the ascii range plus 1
+j label2						# Hex value not in range, try next range
 nne2: j l2
 
-label2: li $t0, 97			# Initialize lower bound for the for the ascii range a - f
-li $t2, 103							# Initialize upper bound for the ascii range a - f
-addi $t4, $zero, 10			# Set temporary variable to 10
+label2: li $t0, 97				# Initialize lower bound for the for the ascii range a - f
+li $t2, 103						# Initialize upper bound for the ascii range a - f
+addi $t4, $zero, 10				# Set temporary variable to 10
 sub $t3, $t0, $t4				# t3 is the value to be subtracted from the ascii value to get its value in hex
 
-l3: bne $a0, $t0, ne3 	# a0 is the ascii value, t0 is the lower bound of the ascii range to be checked
+l3: bne $a0, $t0, ne3 			# a0 is the ascii value, t0 is the lower bound of the ascii range to be checked
 sub $v0, $t0, $t3				# t3 is the value to be subtracted from the ascii value to get its value in hex
-jr $ra									# Return the decimal value
+jr $ra							# Return the decimal value
 ne3: addi $t0, $t0, 1
-bne $t0, $t2, nne3			# t2 is the upper bound of the ascii range plus 1
-jr $ra									# Invalid hex value, return
+bne $t0, $t2, nne3				# t2 is the upper bound of the ascii range plus 1
+jr $ra							# Invalid hex value, return
 nne3: j l3
